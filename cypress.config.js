88ee8a91b67f3bc4c3f1bb7d02473fc6d5c470c2
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-11-05 12:02:08 +0100

    environment variables are added

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      GITHUB_TOKEN: "YOUR ACCESS TOKEN",
+      API_BASE_URL: "https://api.github.com",
+    },
   },
 });
