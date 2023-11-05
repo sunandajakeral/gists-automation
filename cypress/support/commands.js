227b8339f93dc42491d942658bb03df7ae5cdebf
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-11-05 12:03:33 +0100

    custom command added:createGistRequest

---
+++ cypress/support/commands.js
@@ -0,0 +1,22 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+const accessToken = Cypress.env("GITHUB_TOKEN");
+
+Cypress.Commands.add("createGistRequest", (gistData, apiUrl) => {
+  // Make an API request to create a Gist using an access token
+  cy.request({
+    method: "POST",
+    url: apiUrl,
+    headers: {
+      Authorization: `token ${accessToken}`,
+    },
+    body: gistData,
+  });
+});
