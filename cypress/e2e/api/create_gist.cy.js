d4ccd36fbc4f6cf59a7c15ca83e9d7ff0d56bd16
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-11-05 12:08:24 +0100

    test added:TC_02_Create a gist with multiple files

--- cypress/e2e/api/create_gist.cy.js
+++ cypress/e2e/api/create_gist.cy.js
@@ -26,4 +26,33 @@ describe("Create Gist API Tests", () => {
       });
     });
   });
+
+  it("TC_02_Create a gist with multiple files", () => {
+    cy.get("@gistContent").then((gistData) => {
+      gistData.files["test-file-2.js"] = {
+        content: "Welcome to the World!",
+      };
+      cy.createGistRequest(gistData, apiUrl).then((response) => {
+        // Assert that the response status code is 201 (Created)
+        expect(response.status).to.eq(201);
+
+        // Validate the response data
+        expect(response.body.description).to.eq(gistData.description);
+
+        // Check that the response has file content as expected
+        expect(response.body.files["test-file.js"].content).to.deep.equal(
+          gistData.files["test-file.js"].content
+        );
+
+        // Check that the response has file content as expected
+        expect(response.body.files["test-file-2.js"].content).to.deep.equal(
+          gistData.files["test-file-2.js"].content
+        );
+
+        // Store the Gist ID for future reference (you can use it in other tests)
+        const gistId = response.body.id;
+        cy.wrap(gistId).as("createdGistId");
+      });
+    });
+  });
 });
