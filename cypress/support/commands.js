01be7d2d3b5853d5f71d0181ad755d8db7157d6a
Sunanda Jakeral <jakeralbsunanda@gmail.com>
2023-11-05 19:20:46 +0100

    added the command updateGistRequest

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,16 +8,36 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 const accessToken = Cypress.env("GITHUB_TOKEN");
+const apiUrl = Cypress.env("API_BASE_URL");
 
-Cypress.Commands.add("createGistRequest", (gistData, apiUrl,failOnStatusCode= true) => {
-  // Make an API request to create a Gist using an access token
-  cy.request({
-    method: "POST",
-    url: apiUrl,
-    failOnStatusCode: failOnStatusCode,
-    headers: {
-      Authorization: `token ${accessToken}`,
-    },
-    body: gistData,
-  });
-});
+Cypress.Commands.add(
+  "createGistRequest",
+  (gistData, failOnStatusCode = true) => {
+    // Make an API request to create a Gist using an access token
+    cy.request({
+      method: "POST",
+      url: `${apiUrl}/gists`,
+      failOnStatusCode: failOnStatusCode,
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+      body: gistData,
+    });
+  }
+);
+
+Cypress.Commands.add(
+  "updateGistRequest",
+  (gistId, gistData, failOnStatusCode = true) => {
+    // Make an API request to update the Gist
+    cy.request({
+      method: "PATCH",
+      url: `${apiUrl}/gists/${gistId}`,
+      failOnStatusCode:failOnStatusCode,
+      headers: {
+        Authorization: `token ${accessToken}`,
+      },
+      body: gistData,
+    });
+  }
+);
